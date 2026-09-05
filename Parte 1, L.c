1ba11b05f7822f5fda644220ffd35159ed27edf8
Matheus Santos <matheus.barbosa290608@gmail.com>
2026-09-04 22:33:07 -0300

    Add greeting program in Parte 1, L.c

---
+++ Parte 1, L.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string nome;
+	
+	std::cout << "Digite seu nome: ";
+	std::cin >> nome;
+	
+	std::cout << nome <<", Você é especial!" << std::endl;
+	return 0;
+}
